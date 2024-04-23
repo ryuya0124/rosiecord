c49cd039f14dc4942644ef15afe47185106eb93d
ryuya0124 <79310875+ryuya0124@users.noreply.github.com>
2024-04-23 19:58:41 +0900

    constants.js の更新

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_219.1.ipa";
+Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_226.0.ipa";
 export { Shell, Colors, Divider, States, Constants };
